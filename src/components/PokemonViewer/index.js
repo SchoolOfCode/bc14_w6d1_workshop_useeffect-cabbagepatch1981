0161c72b8cfe972983cfbad3d190e0de369d1745
Victoria Burton <vlaburton@gmail.com>
2023-04-17 17:33:44 +0100

    task 1 i think

--- src/components/PokemonViewer/index.js
+++ src/components/PokemonViewer/index.js
@@ -1,20 +1,38 @@
+//   //TODO: Task 1 - send http request to `https://pokeapi.co/api/v2/pokemon/1' and display the data
+//   // TODO: Task 2 - send http request to `https://pokeapi.co/api/v2/pokemon/${id}` and display the data!
+//   // HINT: you will need useState and useEffect!
+//   return <div className="pokemon-viewer">{pokemon && pokemon.name}</div>;
+// }
+
+// export default PokemonViewer;
+
 import { useState, useEffect } from "react";
 
 function PokemonViewer() {
   const [pokemon, setPokemon] = useState(null);
+
   useEffect(() => {
     async function fetchData() {
       const response = await fetch("https://pokeapi.co/api/v2/pokemon/1");
       const data = await response.json();
+      console.log(data); // Log the data to make sure it's working correctly
       setPokemon(data);
     }
     fetchData();
   }, []);
 
-  //TODO: Task 1 - send http request to `https://pokeapi.co/api/v2/pokemon/1' and display the data
-  // TODO: Task 2 - send http request to `https://pokeapi.co/api/v2/pokemon/${id}` and display the data!
-  // HINT: you will need useState and useEffect!
-  return <div className="pokemon-viewer">{pokemon && pokemon.name}</div>;
+  return (
+    <div className="pokemon-viewer">
+      {pokemon && (
+        <div>
+          <h2>{pokemon.name}</h2>
+          <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+          <p>Height: {pokemon.height}</p>
+          <p>Weight: {pokemon.weight}</p>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default PokemonViewer;
